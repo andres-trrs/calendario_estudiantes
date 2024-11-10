ea58cee502a0b74ef8d3ab8904fb669833421773
andres-trrs <andres.torres32@inacapmail.cl>
2024-11-10 17:50:30 -0300

    formulario de registro agregado funcionando

--- calendarioApp/views.py
+++ calendarioApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Usuario
 
-def muestrahome (request):
-    return render (request, 'home.html')
+def muestrahome(request):
+    return render(request, 'home.html')
 
 def login(request):
     if request.method == 'POST':
@@ -16,3 +16,28 @@ def login(request):
         except Usuario.DoesNotExist:
             messages.error(request, "Credenciales incorrectas")
     return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        correo = request.POST.get('correo')
+        contrasena = request.POST.get('contrasena')
+        fecha_nacimiento = request.POST.get('fecha_nacimiento')
+        
+        # Verificar si el correo ya está registrado
+        if Usuario.objects.filter(correo=correo).exists():
+            messages.error(request, "El correo ya está registrado.")
+        else:
+            # Crear el usuario
+            Usuario.objects.create(
+                nombre=nombre,
+                apellido=apellido,
+                correo=correo,
+                contrasena=contrasena,
+                fecha_nacimiento=fecha_nacimiento
+            )
+            messages.success(request, "Usuario registrado exitosamente.")
+            return redirect('login')  # Redirige a la página de login tras registrarse
+    
+    return render(request, 'registration.html')
--- calendario_estudiantes/urls.py
+++ calendario_estudiantes/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from calendarioApp.views import login, muestrahome
+from calendarioApp.views import login, muestrahome, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', login, name='login'),
+    path('register/', register, name='register'),
     path('home/', muestrahome, name='home',)
 ]
